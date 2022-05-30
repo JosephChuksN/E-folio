aa2deecd135c8fd6b411ab7732ec5350f923baed
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-05-30 19:22:52 +0800

    Added Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { React, useState} from "react"
 
 
+  
 function Sidebar(){
     const  sideBarItems = [
         {
@@ -21,23 +22,38 @@ function Sidebar(){
         },
        
     ]
-return(
-     <div className="lg:w-64 lg:fixed p-2 w-3/4 shadow shadow-gray-900/50 h-screen">
-         <div className="p-4 lg:mt-20">
-         <div className="flex justify-between mb-10">
-             <img className="w-1/4 rounded-full h-auto" src="https://picsum.photos/536/354" alt="" />
-             <span className="text-2xl lg:hidden"><ion-icon name="close"></ion-icon></span>
-         </div>
 
-             <ul className="">{
+    const handleShow = ()=>{
+        setShowSideBar(show => !show)
+        console.log('worked')
+    }
+    const [showSideBar, setShowSideBar] = useState(false)
+
+
+return(
+     <nav className="lg:fixed lg:h-20 w-full lg:z-20  ">
+<div className="">
+<div className=" flex justify-between p-5  w-full shadow-sm shadow-gray-500/50">
+        
+        <h1 className="text-2xl font-bold text-blue-600/50 ">MyResume</h1>
+        <span className={`text-4xl text-blue-600 absolute right-2 lg:hidden`} onClick={handleShow}><ion-icon name="menu"></ion-icon></span>
+    </div>
+         <div className={`lg:static lg:z-[-100] lg:mt-[-0.5]  lg:w-64  p-4  absolute bg-gray-50 h-screen top-0 z-10 w-3/4 md:w-64 shadow shadow-gray-900/50 ${showSideBar? "left-0" : "left-[-690px]"} transition-all ease-in duration-300 `}>
+         
+         <div className=" mb-10  lg:hidden">
+             <span className="text-3xl font-bold text-red-800 absolute right-2 top-2 " onClick={handleShow}><ion-icon name="close"></ion-icon></span>
+         </div>
+             <ul className="mt-10">{
               sideBarItems.map((lists, index)=>{
                   return(
-                      <li key={`${lists.icon}_{lists.name}`} className="text-xl my-5 ml-4  p-2 hover:bg-gray-200/50"><span className="mr-4"><ion-icon name={lists.icon}></ion-icon></span>{lists.name}</li>
+                      <li key={`${lists.icon}_{lists.name}`} className="text-xl my-5 ml-4  p-2 hover:bg-gray-200/50"><span className="mr-4"><ion-icon name={lists.icon} ></ion-icon></span>{lists.name}</li>
                   )
               })      
                  }</ul>
          </div>
-     </div>
+</div>
+          
+     </nav>
 
 
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '0 5px 10px -15px rgba(0, 0, 0, 0.3)',
+      }
+    },
   },
   plugins: [],
 }
