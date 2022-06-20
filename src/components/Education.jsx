0ed4b9f154f7150c28ce4403a347b4f682dc19f5
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-06-20 12:10:09 -0600

    modified the education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -5,7 +5,7 @@ function Education(){
    const {educationData, handleEduDataChange } = useContext(AppContext)
    return(
    <div className="text-gray-100 translate-y-20 px-3   pt-36">
-       <div className="lg:w-1/2 p-5 rounded bg-slate-300/10">
+       <div className="lg:w-1/2 lg:p-5 p-2 rounded bg-slate-300/10">
           <div><span  className="flex justify-end text-2xl"><ion-icon  name="close"></ion-icon></span></div>
           {educationData.map((eduData, index) =>  (
            <div key={index} className="flex flex-col gap-5 ">
@@ -31,15 +31,30 @@ function Education(){
             />
           </div>
 
-            <div className="flex gap-10 justify-between">
+            <div className="flex lg:gap-10 justify-between items-center">
              <div className="flex flex-col">
                 <label htmlFor="" >Start Date</label>
-             <input  className="bg-slate-700/50 rounded border-slate-400 border p-2" type="date" />
+             <input  className="bg-slate-700/50 rounded border-slate-400 border p-2" 
+               type="date" 
+               name="startDate" 
+               value={eduData.startDate}
+               id=""
+               onChange={handleEduDataChange}
+             
+             />
              </div>
             
               <div  className="flex flex-col">
                 <label htmlFor="" >End Date</label>
-             <input  className="bg-slate-700/50 rounded border-slate-400 border p-2" type="date" />
+             <input  className="bg-slate-700/50 rounded border-slate-400 border p-2" 
+               type="date" 
+               name="endDate" 
+               value={eduData.endDate}
+               id=""
+               onChange={handleEduDataChange}
+             
+             
+             />
              </div>
             </div>
            </div>
